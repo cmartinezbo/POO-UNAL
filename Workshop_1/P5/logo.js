4941ab58f8e05b458c5e788453e9b51688c89597
Juan Sebastián Martínez B <82240142+jumartinezbo@users.noreply.github.com>
2023-03-13 15:39:58 -0500

    Create logo.js

---
+++ Workshop_1/P5/logo.js
@@ -0,0 +1,25 @@
+function setup() {
+  createCanvas(400, 400);
+}
+
+function draw() {
+  background(255);
+  noStroke();
+
+  // Draw the circle
+  fill('#ffffff');
+  ellipse(200, 200, 200);
+
+  // Draw the crown
+  fill('#fecd0a');
+  triangle(200, 70, 140, 180, 260, 180);
+  triangle(200, 50, 110, 140, 200, 30);
+  triangle(200, 50, 290, 140, 200, 30);
+
+  // Draw the initials
+  fill('#ffffff');
+  rect(160, 160, 80, 40);
+  fill('#000000');
+  textSize(30);
+  text('RM', 175, 190);
+}
