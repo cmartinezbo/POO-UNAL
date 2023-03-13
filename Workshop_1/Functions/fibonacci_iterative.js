4c2d86383ebd873b72cb1c56ff0ed635fdc4d536
Cristian Martínez <cristianmartinez1700@gmail.com>
2023-03-13 14:57:04 -0500

    Update and rename Workshop_1/Functions.js to Workshop_1/Functions/fibonacci_iterative.js

--- Workshop_1/Functions/fibonacci_iterative.js
+++ Workshop_1/Functions/fibonacci_iterative.js
@@ -13,14 +13,3 @@ function fibonacciSequenceIterative(n) {
 let numberIterative = parseInt(prompt('Compute Fibonacci Iterative sequence until:'));
 let resultIterative = fibonacciSequenceIterative(numberIterative);
 console.log(resultIterative);
-
-
-// Fibonacci Resursive
-
-function fibonacciSequenceRecursive(n) {
-    return n >= 45 ? '(Technical difficulties)' : n <= 0 ? 'Enter a valid number' : n === 1 ? 0 : n === 2 ? 1 : fibonacciRecursive(n - 1) + fibonacciRecursive(n - 2);
-  }
-
-let numberRecursive = parseInt(prompt('Compute Fibonacci Recursive sequence until:'));
-let resultRecursive = fibonacciSequenceRecursive(numberRecursive);
-console.log(resultRecursive);
