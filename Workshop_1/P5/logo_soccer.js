209d5d806091a122a8ee9dee6cb0d214f2312cc5
Juan Sebastián Martínez B <82240142+jumartinezbo@users.noreply.github.com>
2023-03-23 21:06:23 -0500

    Update logo_soccer.js

--- Workshop_1/P5/logo_soccer.js
+++ Workshop_1/P5/logo_soccer.js
@@ -1,25 +1,57 @@
+let xA = 120;
+let yA = 100;
+let rectangleAWidth = 100;
+let rectangleAHeight = 25;
+
+let xB = xA + rectangleAWidth - rectangleAHeight;
+let yB = yA + rectangleAHeight;
+let rectangleBWidth = rectangleAHeight;
+let rectangleBHeight = rectangleAWidth + 40;
+
+let xC = xA + rectangleAWidth + rectangleAHeight;
+let yC = yA;
+let rectangleCWidth = rectangleAHeight;
+let rectangleCHeight = rectangleAHeight + rectangleBHeight + 30;
+
+let radiusA = 120;
+let radiusB = 165;
+
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(430, 430);
 }
 
-function draw() {
+function draw() { 
   background(0);
+  
+  // Set text properties
+  textSize(29);
+  fill("#FFFFFF");
+  
+  // Write "Juventus" at (200, 200)
+  text("JUVENTUS", xA, yA - 20);
+
+  // Draw rectangle 1
+  fill(255);
   noStroke();
+  rect(xA, yA, rectangleAWidth, rectangleAHeight);
+  rect(xB, yB, rectangleBWidth, rectangleBHeight);
+  rect(xC, yC, rectangleCWidth, rectangleCHeight);
+  
 
-  // Draw the circle
-  fill('#ffffff');
-  ellipse(200, 200, 200);
-
-  // Draw the crown
-  fill('#fecd0a');
-  triangle(200, 70, 140, 180, 260, 180);
-  triangle(200, 50, 110, 140, 200, 30);
-  triangle(200, 50, 290, 140, 200, 30);
-
-  // Draw the initials
-  fill('#ffffff');
-  rect(160, 160, 80, 40);
-  fill('#000000');
-  textSize(30);
-  text('RM', 175, 190);
+  
+  fill(255);
+  noStroke();
+  arc(xC - (radiusB/2 - rectangleAHeight), yC + rectangleCHeight, radiusB, radiusB, 0, PI*(2.5/4));
+  
+    fill(0);
+  noStroke();
+  arc(xC - (radiusB/2 - rectangleAHeight), yC + rectangleCHeight, radiusB - 50, radiusB - 50, 0, PI*(2.6/4));
+  
+  fill(255);
+    noStroke();
+  arc(xB - (radiusA/2 - rectangleAHeight), yB + rectangleBHeight, radiusA, radiusA, 0, PI*(2.5/4));
+  
+  fill(0);
+  noStroke();
+  arc(xB - (radiusA/2 - rectangleAHeight), yB + rectangleBHeight, radiusA - 50, radiusA - 50, 0, PI*(2.6/4));
 }
